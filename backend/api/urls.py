6acef9f8c429cb1008f8d86ff3a425f65738e25e
Cairé <cairecr@hotmail.com>
2021-06-14 23:38:58 -0300

    Alteração do modelo de dados Escola
    aaaa

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 router = routers.DefaultRouter()
@@ -14,5 +15,11 @@ router.register(r'avaliacao', views.AvaliacaoViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('escolaview/<int:pk>/', views.escolaView),
+    path('novousuario/', views.novoUsuario),
+    path('usuario/', views.perfilUsuario),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
+    path('usuariotoken/', views.usuarioToken)
 ]
